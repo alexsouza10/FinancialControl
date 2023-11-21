a44c120aad0a8d2c61e0ca11badcc03a3edb9b25
alexsouza10 <alexsouza160298@gmail.com>
2023-11-21 20:23:08 -0300

    feat: FinanceControl add

--- Finances/myfinancessystem/src/components/FinanceControl/FinanceControl.tsx
+++ Finances/myfinancessystem/src/components/FinanceControl/FinanceControl.tsx
@@ -1,4 +1,16 @@
-const FinanceControl = () => {
+import { FinanceControlProps } from '../../models/interfaces/FinanceControlProps/FinanceControlProps';
+import { Movement } from '../../models/interfaces/Movement/Movement';
+import './FinanceControl.css';
+
+const FinanceControl = ({
+  handleSetMovement,
+  balance,
+  expenses }: FinanceControlProps) => {
+
+  const receiveNewMovement = (movement: Movement) => {
+    movement && handleSetMovement(movement);
+  };
+
   return (
     <div className="container_finances">
       {/* BALANCE*/}
